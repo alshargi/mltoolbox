5beed05fbbc3c100938620b2122eea7047b3b5f6
Faisal ALSHARGI <alshargi@hotmail.de>
2022-07-28 14:38:43 -0400

    Update __init__.py

--- mltoolbox/__init__.py
+++ mltoolbox/__init__.py
@@ -2,6 +2,7 @@
 # in stead of from ppack.functions import average
 
 
-from .functions import save_file, count_now, read_json_originalText, load_Model_modalityNgram4MaxLsvc_model
+from .functions import save_file, count_now, read_json_originalText, load_modality_Ngram4MaxLsvc_model, load_countvectorizer_Ngram4MaxLsvc_model, load_fidftransformer_Ngram4MaxLsvc_model
+
 
  
