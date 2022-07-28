6979e49352d0b54ec88e7f7385ee7d4d22a69273
Faisal ALSHARGI <alshargi@hotmail.de>
2022-07-28 12:40:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='lextoolbox',
-    version='0.0.2',
+    name='mltoolbox',
+    version='0.0.3',
     author='Faisal Alshargi',
     author_email='falsharg@amazon.com',
     description='Testing installation of Package',
