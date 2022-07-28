e8b16045f1a5c83f63eb2d764eaac3d7d28c402d
Faisal ALSHARGI <alshargi@hotmail.de>
2022-07-28 14:08:47 -0400

    Update functions.py

--- mltoolbox/functions.py
+++ mltoolbox/functions.py
@@ -20,7 +20,7 @@ def save_file(xlist, xxpath):
 
 
 def load_Model_modalityNgram4MaxLsvc():   
-    model_mod = joblib.load("./modality_model.sav")
+    model_mod = joblib.load("mltoolbox/modality_model.sav")
     #vec_mod = joblib.load("https://github.com/alshargi/mltoolbox/blob/5ab89f89bea1610e5cd14186fd09ede70752552b/mltoolbox/modality_countvectorizer.sav")
     #tfidf_transformer_mod= joblib.load("https://github.com/alshargi/mltoolbox/blob/5ab89f89bea1610e5cd14186fd09ede70752552b/mltoolbox/modality_fidftransformer.sav")
     print("Models , loaded ") 
