9291d9dcf15921fc1aabb723bce9348ab90df1c1
Faisal ALSHARGI <alshargi@hotmail.de>
2022-07-28 22:24:27 -0400

    Update __init__.py

--- mltoolbox/__init__.py
+++ mltoolbox/__init__.py
@@ -5,5 +5,4 @@
 from .functions import save_file, count_now, read_json_originalText, load_modelslonk, load_modality_Ngram4MaxLsvc, load_countvectorizer_Ngram4MaxLsvc, load_fidftransformer_Ngram4MaxLsvc
 
 
-
  
