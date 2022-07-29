b94488194c4d919cb66e6b6ab830f3135b1b5c95
Faisal ALSHARGI <alshargi@hotmail.de>
2022-07-29 14:47:54 -0400

    Update __init__.py

--- mltoolbox/__init__.py
+++ mltoolbox/__init__.py
@@ -2,4 +2,4 @@
 # in stead of from ppack.functions import average
 
 
-from .functions import save_file, count_now, read_json_originalText, labels_details, load_modality_model, read_csv_file_tab, read_csv_file_comma, CountFrequency_labeles,log, load_model
+from .functions import save_file, count_now, read_json_originalText, get_predected, labels_details, load_modality_model, read_csv_file_tab, read_csv_file_comma, CountFrequency_labeles,log, load_model
