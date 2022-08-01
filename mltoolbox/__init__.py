81bed173f663412195b70e06f576e70cf6c5a2c0
Faisal ALSHARGI <alshargi@hotmail.de>
2022-08-01 17:00:44 -0400

    Update __init__.py

--- mltoolbox/__init__.py
+++ mltoolbox/__init__.py
@@ -5,3 +5,6 @@
 from .functions import save_file, count_now, read_json_originalText, get_predict, Modality_labels_details
 from .functions import CodeSwitch_labels_details, Types_labels_details, Ranks_labels_details, load_modality_model
 from .functions import read_csv_file_tab, read_csv_file_comma, CountFrequency_labeles,log, load_model
+
+from .functions import show_report, chart_data, make_confusion_matrix, show_wrong_prred
+  
