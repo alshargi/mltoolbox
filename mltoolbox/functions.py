b4316d12b2f2bf7c12343cbb4e24cab311899dd3
Faisal ALSHARGI <alshargi@hotmail.de>
2022-08-11 13:46:43 -0400

    Update functions.py

--- mltoolbox/functions.py
+++ mltoolbox/functions.py
@@ -168,7 +168,8 @@ def CountFrequency_labeles(my_list, inputfile_siz):
 
 
 def load_modality_model(): 
-    models_link = "https://github.com/alshargi/mltoolbox/raw/main/mltoolbox/models.zip"
+    #models_link = "https://github.com/alshargi/mltoolbox/raw/main/mltoolbox/models.zip"
+    models_link = "https://github.com/alshargi/mltoolbox/raw/main/mltoolbox/modality_2lab.zip"
     return models_link
 
 
