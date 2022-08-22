22cd9eef5d7926d8daa844e5a1b80524f744530e
Faisal ALSHARGI <alshargi@hotmail.de>
2022-08-22 09:39:08 -0400

    Update __init__.py

--- mltoolbox/__init__.py
+++ mltoolbox/__init__.py
@@ -2,7 +2,7 @@
 # in stead of from ppack.functions import average
 
 
-from .gtd_check_QA import check_GTD_for_CR, load_txt_file, load_txt_file_utf8
+from .gtd_check_QA import check_GTD_for_CR, load_txt_file
 from .functions import load_txt_file, save_file, count_now, read_json_originalText, get_predict, Modality_labels_details
 from .functions import CodeSwitch_labels_details, Types_labels_details, Ranks_labels_details, load_modality_model, load_txt_file_ISO
 from .functions import read_csv_file_tab, read_csv_file_comma, CountFrequency_labeles,log, load_model, load_txt_file_utf8
