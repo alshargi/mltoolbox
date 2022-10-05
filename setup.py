f3ec3325fff72700b6fd10929b5848d262cddd0f
Faisal ALSHARGI <alshargi@hotmail.de>
2022-10-05 14:26:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='mltoolbox',
-    version='0.0.105',
+    version='0.0.106',
     author='Faisal Alshargi',
     author_email='falsharg@amazon.com',
     description='Testing installation of Package',
