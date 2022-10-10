82929a01888407910d754132e6ebe3fdee6807ea
Faisal ALSHARGI <alshargi@hotmail.de>
2022-10-10 12:23:40 -0400

    Update evaluations.py

--- evaluations.py
+++ evaluations.py
@@ -471,6 +471,8 @@ def Create_model_max(Model_used, Training_file, delimiter,ngram_range, analyzer_
     X_test_transformed = tf_transformer.transform(X_test_counts)
     
     labels = LabelEncoder()
+    print("Dataset Labels: ", labels.classes_)
+
     
     y_train_labels_fit = labels.fit(y_train)
     y_train_lables_trf = labels.transform(y_train)
